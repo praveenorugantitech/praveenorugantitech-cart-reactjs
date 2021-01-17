f029dec29e48adad89d7bc4821cc2bda165a0616
Praveen Oruganti <praveenorugantitech@gmail.com>
2021-01-18 02:00:11 +0530

    All About React JS

---
+++ README.md
@@ -0,0 +1,10 @@
+# Cart developed using React JS with Hooks
+
+![screenshot of the app](https://raw.githubusercontent.com/praveenorugantitech/praveenorugantitech-reactjs/master/0_Projects/praveenorugantitech-cart/src/images/screenshot.PNG "Cart")
+
+
+[Live Demo](https://praveenorugantitech-cart.firebaseapp.com/)
+
+
+
+
---
+++ src/data.js
@@ -0,0 +1,36 @@
+export default [
+  {
+    id: 1,
+    title: "Apple iPhone 11 (64GB) - White",
+    price: 63999,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/51o5RmQtroL._SL1024_.jpg",
+    amount: 1,
+  },
+  {
+    id: 2,
+    title:
+      "Panasonic 6 Kg 5 Star Fully-Automatic Top Loading Washing Machine (NA-F60LF1HRB, Grey)",
+    price: 12999,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/71m7U364CDL._SL1500_.jpg",
+    amount: 1,
+  },
+  {
+    id: 3,
+    title: "Amazon Echo (3rd Gen) – Improved sound, powered by Dolby (Black)",
+    price: 9749,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/61XclfIKcbL._SL1000_.jpg",
+    amount: 1,
+  },
+  {
+    id: 4,
+    title:
+      "Samsung 23 L Solo Microwave Oven (MS23F301TAK/TL, Black)New Apple iPad Pro (12.9-inch, Wi-Fi, 128GB) - Silver (4th Generation)",
+    price: 5750,
+    img:
+      "https://images-na.ssl-images-amazon.com/images/I/61RZcuVxPFL._SL1500_.jpg",
+    amount: 1,
+  },
+];
